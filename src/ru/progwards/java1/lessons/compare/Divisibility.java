5c6774285459f9334d5482c05a53e3ed048ebc42
zmb <serg_zh@yahoo.com>
2023-03-12 18:27:33 +0300

    H13 - 4

--- src/ru/progwards/java1/lessons/compare/Divisibility.java
+++ src/ru/progwards/java1/lessons/compare/Divisibility.java
@@ -6,11 +6,11 @@ public class Divisibility {
         if (num2 != 0) {
             if (num1 % num2 == 0) {
                 System.out.println(num1 + " делится нацело на " + num2);
-                System.out.println("Частное от деления " + num1 + " на " + num2 + " равен " + num1 / num2);
+                System.out.println("Частное от деления " + num1 + " на " + num2 + " равно " + num1 / num2);
             }
             else {
                 System.out.println("Остаток от деления " + num1 + " на " + num2 + " равен " + num1 % num2);
-                System.out.println("Частное от деления " + num1 + " на " + num2 + " равен " + div);
+                System.out.println("Частное от деления " + num1 + " на " + num2 + " равно " + div);
             }
         }
         else System.out.println("На ноль делить нельзя");
