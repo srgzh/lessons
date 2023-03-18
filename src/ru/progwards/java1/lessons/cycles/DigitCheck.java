8148bd276fe1a484cdbe937705320bc0cb9ab099
zmb <serg_zh@yahoo.com>
2023-03-18 21:38:21 +0300

    H14 - 2

--- src/ru/progwards/java1/lessons/cycles/DigitCheck.java
+++ src/ru/progwards/java1/lessons/cycles/DigitCheck.java
@@ -2,12 +2,13 @@ package ru.progwards.java1.lessons.cycles;
 
 public class DigitCheck {
     public static boolean containsDigit(int number, int digit) {
-        for (int i = 1; i < number * 10; i *= 10) {
+        if (number == 0 & digit == 0) return true;
+        for (int i = 1; i < number; i *= 10) {
             if ((number % (10 * i) - number % i) == digit * i) return true;
         }
         return false;
     }
     public static void main(String[] args) {
-        System.out.println(containsDigit(41235, 6));
+        System.out.println(containsDigit(12345, 0));
     }
 }
